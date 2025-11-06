06e31ebddab57632113e71ec71b219b008d3ed27
aboucher <amaury.robert.boucher@gmail.com>
2025-11-06 14:05:15 +0100

    Nettoyage complet du dépôt pour héberger uniquement les releases

--- utils/add_adh_act_control.py
+++
@@ -1,7 +0,0 @@
-def adh_act_001(self):
-    new_nb = int(self.nb_edit.text()) + self.act.nb_participant_activite
-    nb_max = self.act.nb_participant_max_activite
-    if new_nb > nb_max:
-        return "ADH_ACT_001"
-    else:
-        return ""
